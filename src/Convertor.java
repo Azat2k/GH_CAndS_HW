2e91e3614498b4b1e469cab861664f6767958587
Azat2k <dzhanov.azat@gmail.com>
2024-04-16 00:44:17 -0500

    .

--- src/Convertor.java
+++ src/Convertor.java
@@ -17,13 +17,4 @@ public class Convertor {
     }
 
 }
-//4.4 Number system conversions:
-//        1. 0x1AF -> binary
-//2. 11011110 -> hex
-//3. 0x8E -> binary
-//4. 31,301 -> hex
-//5. 0x2F + 0x5C -> hex
-//6. 0x8A - 0x1C -> hex
-//7. 1011 * 1101 -> hex
-//8. 0x3E & 0x51 -> hex
-//9. 0x34E1 | 0x12FF -> hex
+
