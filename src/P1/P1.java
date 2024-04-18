8150a4dfa4a7eed9a3b0873301d28e870196d583
Azat2k <dzhanov.azat@gmail.com>
2024-04-17 22:01:52 -0700

    .

---
+++ src/P1/P1.java
@@ -0,0 +1,29 @@
+package P1;
+import java.io.*;
+
+public class P1 {
+    public static void main(String[] args) throws Exception
+    {
+
+        // File path is passed as parameter
+        File file = new File(
+                "C:\\Users\\pankaj\\Desktop\\test.txt");
+
+        // Note:  Double backquote is to avoid compiler
+        // interpret words
+        // like \test as \t (ie. as a escape sequence)
+
+        // Creating an object of BufferedReader class
+        BufferedReader br
+                = new BufferedReader(new FileReader(file));
+
+        // Declaring a string variable
+        String st;
+        // Condition holds true till
+        // there is character in a string
+        while ((st = br.readLine()) != null)
+
+            // Print the string
+            System.out.println(st);
+    }
+}
