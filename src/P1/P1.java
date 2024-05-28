e631ed21e0fe20943f36f690e2db6d2bf5842b68
Azat2k <dzhanov.azat@gmail.com>
2024-05-27 23:13:06 -0500

    .

--- src/P1/P1.java
+++ src/P1/P1.java
@@ -7,15 +7,11 @@ public class P1 {
 
         // File path is passed as parameter
         File file = new File(
-                "C:\\Users\\pankaj\\Desktop\\test.txt");
+                "C:\\Users\\dzhan\\OneDrive\\Рабочий стол\\Черновик.txt");
 
-        // Note:  Double backquote is to avoid compiler
-        // interpret words
-        // like \test as \t (ie. as a escape sequence)
 
         // Creating an object of BufferedReader class
-        BufferedReader br
-                = new BufferedReader(new FileReader(file));
+        BufferedReader br = new BufferedReader(new FileReader(file));
 
         // Declaring a string variable
         String st;
