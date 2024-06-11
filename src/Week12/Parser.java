9ce1d156525a743a1efc9d4ab26e1ce28880ed58
Azat2k <dzhanov.azat@gmail.com>
2024-06-10 23:29:47 -0600

    .

---
+++ src/Week12/Parser.java
@@ -0,0 +1,24 @@
+package Week12;
+
+public class Parser {
+    abstract class ASTNode {
+
+    }
+    class Number extends ASTNode {
+        Token numberToken;
+        public Number(Token numberToken){
+            this.numberToken = numberToken;
+        }
+    }
+
+    class BinaryOp extends ASTNode{
+        ASTNode left;
+        ASTNode right;
+        Token operationToken;
+        public BinaryOp(ASTNode left, ASTNode right, Token operationToken) {
+            this.left = left;
+            this.right = right;
+            this.operationToken = operationToken;
+        }
+    }
+}
